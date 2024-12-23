57746cf3d8667abe3ddc5c9061054806e6b97ff5
__antti__ <antti.heroja@gmail.com>
2024-12-23 21:55:46 +0000

    feat(basic-setup): initial basic setup which but the shapes of the tool on the paper (#1)

--- src/cli.ts
+++ src/cli.ts
@@ -24,21 +24,16 @@ async function formatSchema(schemaPath: string) {
 
 // Command-line argument parsing
 program
-  .name('graphql-linter')
-  .description('CLI tool for linting and formatting GraphQL schemas')
+  .name('graphql-schema-policy-validator')
+  .description('CLI tool for validating your schema policy')
   .version('0.1.0')
 
 program
-  .command('lint')
-  .argument('<schemaPath>', 'Path to the GraphQL schema file')
-  .description('Lint the specified GraphQL schema')
+  .command('validate')
+  .alias('v')
+  .argument('<schemaPath>', 'Path to the GraphQL schema file(s)')
+  .description('Validate the specified GraphQL schema policy')
   .action(lintSchema)
-
-program
-  .command('format')
-  .argument('<schemaPath>', 'Path to the GraphQL schema file')
-  .description('Format the specified GraphQL schema')
-  .action(formatSchema)
 
 program.parse(process.argv)
 
