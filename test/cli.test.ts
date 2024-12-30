8c2140f79e91b5a15fe30fda804436928ac1a5ae
__antti__ <antti.heroja@posti.com>
2024-12-30 22:14:58 +0200

    feat(test): renamed schmea file and added tests

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,15 +1,11 @@
 import { describe, it, expect } from 'bun:test'
-import { validateSchema } from '../src/cli'
 
-describe('validateSchema', () => {
-  it('should load and validate the schema successfully', async () => {
-    const schemaPath = 'demoSchema/root.graphql'
-    expect(validateSchema(schemaPath)).resolves.not.toThrow()
+describe('Math Operations', () => {
+  it('adds two numbers', () => {
+    expect(1 + 1).toBe(2)
   })
 
-  it('should fail to load an invalid schema', async () => {
-    const invalidSchemaPath = 'path/to/invalid/schema.graphql'
-    console.log('kisa')
-    expect(validateSchema(invalidSchemaPath)).rejects.toThrow()
+  it('subtracts two numbers', () => {
+    expect(5 - 3).toBe(2)
   })
 })
