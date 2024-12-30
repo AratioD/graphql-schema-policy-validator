81a7fd8bbb33083904f90cce0e0b003f0329c55d
__antti__ <antti.heroja@gmail.com>
2024-12-30 11:41:10 +0200

    Feat/subscription type documenation rule added (#4)
    * feat(field-subscription): refactored code and now it shows correct row number as well
    
    * feat(subscription type): added support for subscription type
    
    * feat(biome): changed biome settings

--- src/validate/documentation.ts
+++ src/validate/documentation.ts
@@ -1,44 +1,69 @@
-import type { GraphQLSchema, GraphQLObjectType } from "graphql";
+import type { GraphQLObjectType, GraphQLSchema } from "graphql"
 
+export const validateSubscriptionType = async (schema: GraphQLSchema) => {
+  const errors: string[] = []
+  console.log(
+    "ℹ️ Validating properties of the GraphQL subscription type for the schema...",
+  )
 
-export const validateSubscriptionFields = (schema: GraphQLSchema) => {
-	console.log("ℹ️ Validating properties of the GraphQL subscription type for the schema...");
+  const comment =
+    schema.getSubscriptionType()?.astNode?.name?.loc?.startToken?.prev?.prev
+      ?.value
 
-	const subscriptionType = schema.getSubscriptionType();
-	if (!subscriptionType) {
-		console.log("Subscription type is not defined in the schema.");
-		return;
-	}
+  if (!comment) {
+    const rowNumber =
+      schema.getSubscriptionType()?.astNode?.name?.loc?.startToken?.prev?.line
 
-	const errors = validateFields(subscriptionType);
-	handleErrors(errors);
-};
+    errors.push(
+      `Type: subscription is missing description from row → ${rowNumber}`,
+    )
+    handleErrors(errors)
+  }
+}
+
+export const validateSubscriptionFields = async (schema: GraphQLSchema) => {
+  console.log(
+    "ℹ️ Validating properties of the GraphQL subscription fields for the schema...",
+  )
+
+  const subscriptionType = schema.getSubscriptionType()
+  if (!subscriptionType) {
+    console.log("Subscription type fields is not defined in the schema.")
+    return
+  }
+
+  const errors = validateFields(subscriptionType)
+  handleErrors(errors)
+}
 
 const validateFields = (subscriptionType: GraphQLObjectType): string[] => {
-	const errors: string[] = [];
-	const fields = subscriptionType.getFields();
+  const errors: string[] = []
+  const fields = subscriptionType.getFields()
 
-	Object.keys(fields).forEach((fieldName) => {
-		const field = fields[fieldName];
-		const astNode = field.astNode;
-		if (astNode?.loc?.source?.body) {
-			const isDocumentation = astNode?.name?.loc?.startToken?.prev?.kind;
-			if (isDocumentation !== "Comment") {
-				errors.push(`Field: "${subscriptionType}.${fieldName}" is missing description`);
-			}
-		}
-	});
+  for (const fieldName in fields) {
+    if (Object.prototype.hasOwnProperty.call(fields, fieldName)) {
+      const fieldNode = fields[fieldName].astNode
+      const isComment = fieldNode?.name?.loc?.startToken?.prev?.kind
+      if (isComment !== "Comment") {
+        const rowNumber = fieldNode?.loc?.startToken?.line
+        errors.push(
+          `Field: "${subscriptionType}.${fieldName}" is missing description from row → ${rowNumber}`,
+        )
+      }
+    }
+  }
 
-	return errors;
-};
+  return errors
+}
 
 const handleErrors = (errors: string[]) => {
-	if (errors.length > 0) {
-		console.error("❌ Documentation validation failed:");
-		errors.forEach((error) => console.error(`  - ${error}`));
-		process.exit(1);
-	} else {
-		console.log("✅ Documentation validation passed.");
-	}
-};
-
+  if (errors.length > 0) {
+    console.error("❌ Documentation validation failed:")
+    for (const error of errors) {
+      console.error(`  - ${error}`)
+    }
+    process.exit(1)
+  } else {
+    console.log("✅ Documentation validation passed.")
+  }
+}
